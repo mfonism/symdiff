ff4cc09d67448c6550de862c2125ca4a292a7c7a
Mfon Eti-mfon <mfonetimfon@gmail.com>
2021-08-13 22:20:57 +0100

    Rename things in tests

--- tests.py
+++ tests.py
@@ -9,27 +9,27 @@ from .main import Constant, Line, Term
 @pytest.mark.parametrize("var", ("x", "y"))
 @pytest.mark.parametrize("pow", (0, 1, 2))
 def test_term_from_parts_with_coefficient_zero(coeff, var, pow):
-    term = Term.from_parts(coeff, var, pow)
-    assert isinstance(term, Constant)
-    assert term.value == 0
+    const = Term.from_parts(coeff, var, pow)
+    assert isinstance(const, Constant)
+    assert const.value == 0
 
 
 @pytest.mark.parametrize("coeff", (0, 1, 2))
 @pytest.mark.parametrize("var", ("x", "y"))
 @pytest.mark.parametrize("pow", (0,))
 def test_term_from_parts_with_power_zero(coeff, var, pow):
-    term = Term.from_parts(coeff, var, pow)
-    assert isinstance(term, Constant)
-    assert term.value == coeff
+    const = Term.from_parts(coeff, var, pow)
+    assert isinstance(const, Constant)
+    assert const.value == coeff
 
 
 @pytest.mark.parametrize("coeff", (1,))
 @pytest.mark.parametrize("var", ("x", "y", "z"))
 @pytest.mark.parametrize("pow", (1,))
 def test_term_from_parts_with_coefficient_and_power_one(coeff, var, pow):
-    term = Term.from_parts(coeff, var, pow)
-    assert isinstance(term, Line)
-    assert term.variable == var
+    line = Term.from_parts(coeff, var, pow)
+    assert isinstance(line, Line)
+    assert line.variable == var
 
 
 @pytest.mark.parametrize("coeff, pow", ((1, 2), (1, 3), (2, 1), (2, 2)))
